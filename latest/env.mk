5789b1bd0cd9023317f2aaa1bd5e06d21daa56d9
Maël Le Garrec <mlegarrec@online.net>
2018-05-02 09:16:57 +0000

    upgrade to new build system

---
+++ latest/env.mk
@@ -0,0 +1,6 @@
+IMAGE_NAME =			openvpn
+IMAGE_VERSION =		latest
+IMAGE_VERSION_ALIASES =	
+IMAGE_TITLE =			OpenVPN
+IMAGE_DESCRIPTION =		OpenVPN
+IMAGE_SOURCE_URL =		https://github.com/scaleway-community/scaleway-openvpn
